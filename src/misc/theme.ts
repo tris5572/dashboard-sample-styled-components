c29b009cf5c7be02d35081383a23fafacd2c89d2
tris <nexus4fortune@gmail.com>
2023-11-12 16:18:57 +0900

    環境構築

---
+++ src/misc/theme.ts
@@ -0,0 +1,17 @@
+export const THEME = {
+  colors: {
+    text: {
+      main: "#333",
+      light: "#999",
+    },
+    primary: {
+      main: "#77dddd",
+      dark: "#3c8080",
+      light: "#bbf2f2",
+    },
+    background: {
+      light: "#e6f7f7",
+      dark: "#547d7d",
+    },
+  },
+};
